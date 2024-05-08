936f4fa49260d9ed2851afbb1822aa6ac5517adb
Kenneth Duverge <kenneth.duverge@icloud.com>
2024-05-08 17:09:11 -0500

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import QRCode from 'react-qr-code';
+import VCard from 'vcard-creator';
+
+const myVCard = new VCard();
+
+// Some variables
+const lastname = 'Duverge';
+const firstname = 'Kenneth';
+const additional = '';
+const prefix = '';
+const suffix = '';
+
+myVCard
+  // Add personal data
+  .addName(lastname, firstname, additional, prefix, suffix)
+  // Add work data
+  .addCompany('Vimeo')
+  .addJobtitle('Web Developer')
+  .addRole('Software Engineer')
+  .addEmail('kenneth.duverge@icloud.com')
+  .addPhoneNumber(9175871799, 'PREF;WORK')
+  .addSocial('https://twitter.com/kenduve', 'Instagram', 'kenduve')
+  .addURL('https://kennethduverge.com');
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen w-full justify-center items-center gap-8">
+      <form className="w-[400px] h-[400px] border p-4"></form>
+      <div className="w-[400px] h-[400px] border p-4">
+        <QRCode
+          size={256}
+          className="h-auto w-[200px] self-start"
+          value={myVCard.toString()}
+          viewBox={`0 0 256 256`}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
